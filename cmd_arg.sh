108a21e4d63f3e9a72ecc66eda941fe3382127e9
EC2 Default User <ec2-user@ip-172-31-13-33.us-west-1.compute.internal>
2024-04-16 15:18:40 +0000

    added changes for command line argumments

---
+++ cmd_arg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Maintainer : Shreyansh Gandhi
+
+#This script checks for the command line arguments
+
+
+
+echo "Script name : $0"
+echo "First Argument : $1"
+echo "Second Argument : $2"
+echo "Third Argument : $3"
